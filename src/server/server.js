97f19523a61ee37b46fef5bb5e320b358522bd04
RajChoudhary007007 <raaz02256@gmail.com>
2025-08-23 23:37:33 +0530

    new_traking_server

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,8 @@
 // server.js
 
-// const SERVER_API_URL = 'https://backend.matteo-bianchi.com'; 
+const SERVER_API_URL = 'https://backend.matteo-bianchi.com'; 
 const SERVER_URL = 'https://matteo-bianchi.com'; 
-const SERVER_API_URL = 'http://localhost:8000'; 
+// const SERVER_API_URL = 'http://localhost:8000'; 
 
 
 export { SERVER_API_URL, SERVER_URL };
